ec9f2508d1bf2df0de6f6814ea8e8fa5c9a9fe6c
Hugo Riou--Bourdoncle <hrioubourdoncle@gmail.com>
2024-12-01 17:22:21 +0100

    Update summary style

--- src/lib/Summary.svelte
+++ src/lib/Summary.svelte
@@ -52,44 +52,45 @@
 </script>
 
 
-<table class="table-auto border border-slate-500">
+<table class="border border-gray-300 rounded-md border-separate border-spacing-0">
 	<thead>
 	<tr>
-		<th class="border border-slate-600 py-2 px-3">Summary</th>
-		<th class="border border-slate-600  py-2 px-3">LMNP Micro-BIC</th>
-		<th class="border border-slate-600  py-2 px-3">LMNP Régime Réel</th>
+		<th class="border-r border-b border-gray-300 py-2 px-3">Summary</th>
+		<th class="border-r border-b border-gray-300  py-2 px-3">LMNP Micro-BIC</th>
+		<th class="border-b border-gray-300  py-2 px-3">LMNP Régime Réel</th>
 	</tr>
 	</thead>
 	<tbody>
 	<tr>
-		<td class="border border-slate-600 p-2 text-md font-semibold text-gray-700">Total Rent</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryBicSystem.rent}€</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryRealSystem.rent}€</td>
+		<td class="border-r border-b border-gray-300 p-2 text-md font-semibold text-gray-700">Total Rent</td>
+		<td class="border-r border-b border-gray-300 p-2 text-indigo-600 font-semibold">{summaryBicSystem.rent}€</td>
+		<td class="border-b border-gray-300 p-2 text-indigo-600 font-semibold">{summaryRealSystem.rent}€</td>
 	</tr>
 	<tr>
-		<td class="border border-slate-600 p-2 text-md font-semibold text-gray-700">Monthly Loan</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryBicSystem.loan}€</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryRealSystem.loan}€</td>
+		<td class="border-r border-b border-gray-300 p-2 text-md font-semibold text-gray-700">Monthly Loan</td>
+		<td class="border-r border-b border-gray-300 p-2 text-indigo-600 font-semibold">{summaryBicSystem.loan}€</td>
+		<td class="border-b border-gray-300 p-2 text-indigo-600 font-semibold">{summaryRealSystem.loan}€</td>
 	</tr>
 	<tr>
-		<td class="border border-slate-600 p-2 text-md font-semibold text-gray-700">Total Expenses</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryBicSystem.expenses}€</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryRealSystem.expenses}€</td>
+		<td class="border-r border-b border-gray-300 p-2 text-md font-semibold text-gray-700">Total Expenses</td>
+		<td class="border-r border-b border-gray-300 p-2 text-indigo-600 font-semibold">{summaryBicSystem.expenses}€</td>
+		<td class="border-b border-gray-300 p-2 text-indigo-600 font-semibold">{summaryRealSystem.expenses}€</td>
 	</tr>
 	<tr>
-		<td class="border border-slate-600 p-2 text-sm font-semibold text-gray-500">Of Which Taxes</td>
-		<td class="border border-slate-600 p-2 text-indigo-500 font-semibold text-sm">{summaryBicSystem.taxes}€</td>
-		<td class="border border-slate-600 p-2 text-indigo-500 font-semibold text-sm">{summaryRealSystem.taxes}€</td>
+		<td class="border-r border-b border-gray-300 p-2 text-sm font-semibold text-gray-500">Of Which Taxes</td>
+		<td class="border-r border-b border-gray-300 p-2 text-indigo-500 font-semibold text-sm">{summaryBicSystem.taxes}€
+		</td>
+		<td class="border-b border-gray-300 p-2 text-indigo-500 font-semibold text-sm">{summaryRealSystem.taxes}€</td>
 	</tr>
 	<tr>
-		<td class="border border-slate-600 p-2 text-md font-semibold text-gray-700">Cash Flow (Monthly)</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryBicSystem.cashFlow}€</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryRealSystem.cashFlow}€</td>
+		<td class="border-r border-b border-gray-300 p-2 text-md font-semibold text-gray-700">Cash Flow (Monthly)</td>
+		<td class="border-r border-b border-gray-300 p-2 text-indigo-600 font-semibold">{summaryBicSystem.cashFlow}€</td>
+		<td class="border-b border-gray-300 p-2 text-indigo-600 font-semibold">{summaryRealSystem.cashFlow}€</td>
 	</tr>
 	<tr>
-		<td class="border border-slate-600 p-2 text-md font-semibold text-gray-700">Cash Flow (Yearly)</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryBicSystem.yearlyCashFlow}€</td>
-		<td class="border border-slate-600 p-2 text-indigo-600 font-semibold">{summaryRealSystem.yearlyCashFlow}€</td>
+		<td class="border-r border-gray-300 p-2 text-md font-semibold text-gray-700">Cash Flow (Yearly)</td>
+		<td class="border-r border-gray-300 p-2 text-indigo-600 font-semibold">{summaryBicSystem.yearlyCashFlow}€</td>
+		<td class="p-2 text-indigo-600 font-semibold">{summaryRealSystem.yearlyCashFlow}€</td>
 	</tr>
 	</tbody>
 </table>
